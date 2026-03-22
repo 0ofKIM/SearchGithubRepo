f69ba4fcfc99326bea6c38b35ed62ed90bcd82e1
0ofKim <pos00042@naver.com>
2026-03-22 16:09:01 +0900

    Create Project

---
+++ SearchGithubRepo/SearchGithubRepo/SearchGithubRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchGithubRepoApp.swift
+//  SearchGithubRepo
+//
+//  Created by 0ofKim on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct SearchGithubRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
